544c345fc2ffbd01aa3e5736bb764736dc38d56b
wangjie <jie.wang@qizhidao.com>
2020-06-06 15:34:51 +0800

    prettier代码格式校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,28 @@ module.exports = {
   env: {
     node: true
   },
+  env: { // eg如果不配置browser，window就会被eslint报undefined的错
+    "es6": true,
+    "browser": true,
+    "node": true
+  },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'plugin:prettier/recommended'
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
+  "parser": "babel-eslint",// 解决ES6 improt会报错
+  "plugins": ["react", "jsx-a11y", "import"],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
+
